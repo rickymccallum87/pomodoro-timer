8725735b0cc31e5aaedbbbc21cef3e589ac73d67
learnsometing <brianmonaccio@gmail.com>
2019-01-31 09:49:54 -0500

    Wired up the buttons and started working on the timer function

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
+eventListeners();
+
+function eventListeners(){
+    let buttons = document.querySelectorAll('button');
+    buttons.forEach(button => {
+        button.addEventListener('click', (e) =>{
+            clicked(button);
+        });
+    });
+}
+
+function clicked(button){
+    let buttonText = button.innerText;
+
+    switch(buttonText){
+        case 'Pomodoro':
+            timer(0, 25, 0);
+            break;
+    }
+}
+
 // Pseudo:
 // User presses 'Pomodoro' button
 // minutes = 25 is sent to timer()
@@ -5,24 +26,22 @@
 // Then each second, display difference between that future time
 // and the current time in #timer.textContent
 
-function timer(minutes) {
-    let date = new Date(),
-    m = date.setMinutes(minutes),
-    seconds = date.getSeconds();
+function timer(hours, minutes, seconds){
+    let targetTime = new Date(hours, minutes, seconds).getTime(),
+    now = new Date().getTime(),
+    interval = targetTime - now;
+    // Pad the time if it is a single digit number, to retain the correct formatting.
+    // function checkTime(i) {
+    //     if (i < 10) {
+    //         i = '0' + i;
+    //     }
+    //     return i;
+    // }
+    //Calculations
+    var hrs = Math.floor(interval/(60*60*1000));
+    var mins = Math.floor(interval/(60*1000));
+    var secs = Math.floor(interval/1000);
 
-    m = checkTime(m);
-    seconds = checkTime(seconds);
-
-    document.getElementById('timer').innerText = minutes + ':' + seconds;
-
-    // Call timer() after 500ms
-    let time = setTimeout(timer, 500);
+    document.getElementById('timer').innerText = hrs + ":" + mins + ':' + secs;
 }
 
-// Pad the time if it is a single digit number, to retain the correct formatting.
-function checkTime(i) {
-    if (i < 10) {
-        i = '0' + i;
-    }
-    return i;
-}
